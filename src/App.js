994bada245ad3d228a325297ddaae4adbd5bc197
PAAWSA02LC0900 <Duoc@PAAWSA02LC0900.duoc.acad>
2025-11-21 22:23:16 -0300

    avances programacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Usuarios from './pages/users';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/usuarios' element={<Usuarios/>}></Route>
       </Routes>
     </Router>    
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import NavBarPrincipal from './components/navbar';
+import Sidemenu from './components/sidemenu';
+import Kpis from './components/kpis';
 
 function HomePrincipal() {
     return (
         <div>
             <NavBarPrincipal/>
+            <Sidemenu/>
+            <Kpis/>
         </div>
     );
 }
